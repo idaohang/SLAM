729da3676a0095eba18cc5aaec5c7deaa3ae80b5
Kevin George <kevingeorgetm@gmail.com>
2013-02-07 15:48:48 -0600

    Nodes can be deleted.

--- llist.c
+++ llist.c
@@ -3,24 +3,34 @@
 #include "llist.h"
 
 lnode NULLED = {NULL, NULL};
+const unsigned long int LNODEP_SIZE = sizeof(lnode*);
 
 void init_list(lnode **head)
+// USAGE: lnode *head; init_list(&head);
+// All new linked lists must be initialized with this function.
 { *head = &NULLED; }
 
-void prepend(lnode **head, void *data)
+int prepend(lnode **head, void *data)
+// USAGE: int a; prepend(&head, &a);
+// Insert a node at the beginning of a list. head will be changed.
 {
+	if (!*head) return 1;
 	if (!data) fprintf(stderr, "Warning: null data pointer.");
-	lnode *add = malloc(sizeof(lnode));
+	lnode *add = malloc(LNODEP_SIZE);
 	lnode *nhead = *head;
 	add->data = data;
 	add->next = nhead->next;
 	nhead->next = add;
 	nhead->data = NULL;
 	*head = nhead;
+	return 0;
 }
 
-void append(lnode *head, void *data)
+int append(lnode *head, void *data)
+// USAGE: int a; append(head, &a);
+// Insert a node at the tail of the linked list.
 {
+	if (!head) return 1;
 	if (!data) fprintf(stderr, "Warning: null data pointer.");
 	lnode *last = &(*head);
 	while(get_next(last, &last) == 0);
@@ -28,15 +38,24 @@ void append(lnode *head, void *data)
 	add->data = data;
 	add->next = NULL;
 	last->next = add;
+	return 0;
 }
+
+int delete_node(lnode *del)
+{ *del = *del->next; }
+
+int delete_next(lnode *prev)
+{ *prev->next = *prev->next->next; }
 
 int get_next(lnode *head, lnode **target)
 {
+	if (!head) return 1;
 	if (head->next == NULL) return 1;
 	*target = head->next;
 	return 0;
 }
 
+/*
 int main(void)
 {
 	int i, a = 50;
@@ -64,7 +83,11 @@ int main(void)
 	lnode *change = curr->next->next->next;
 	change->data = &a;
 	while(get_next(curr, &curr) == 0)
+	{
+		if (*(int*)curr->data == 6) delete_next(curr);
+		if (*(int*)curr->data == 3) delete_node(head,curr);
 		printf("%d %p\n", *(int*)curr->data, curr);
-	/**/
+	}
 	return 0;
 }
+*/
--- llist.h
+++ llist.h
@@ -6,3 +6,10 @@ struct lnode_t {
 };
 
 typedef struct lnode_t lnode;
+
+void init_list(lnode **head);
+int prepend(lnode **head, void *data);
+int append(lnode *head, void *data);
+int get_next(lnode *head, lnode **target);
+int delete_node(lnode *del);
+int delete_next(lnode *prev);
